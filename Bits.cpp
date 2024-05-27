6641283e6666304ee8a3f76e15fccdd923ec1c9b
Siraj Chowdhury <88205373+SirajChowdhuryofficial@users.noreply.github.com>
2024-05-28 00:17:39 +0600

    Add files via upload

---
+++ Bits.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+
+
+int main() {
+
+    Mostafa
+
+    // https://codeforces.com/contest/484/problem/A
+
+    w(x) {
+
+        ll l, r; cin >> l >> r;
+
+        for (int i = 0; i < 61; ++i) {
+
+            if ((l | (1ll << i)) <= r) {
+                l |= (1ll << i);
+            }
+        }
+
+        cout << l << endl;
+    }
+
+    return 0;
+}
---
+++ Brick_Walls.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+
+int main() {
+
+    Mostafa
+
+    // https://codeforces.com/contest/1918/problem/A
+
+    w(x) {
+
+        ll n, m; cin >> n >> m;
+
+        m /= 2;
+
+        cout << n * m << endl;
+    }
+
+    return 0;
+}
